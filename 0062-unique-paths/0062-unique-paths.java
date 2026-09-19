aca31fb6b144741266ecd0c0c659ae4a02918c74
Ayush <ap4769883@gmail.com>
2026-09-19 16:17:13 +0530

    Time: 0 ms (100%), Space: 42.3 MB (28.53%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,30 +1,24 @@
 class Solution {
     public int uniquePaths(int m, int n) {
+        // Create a 2D DP array filled with zeros
         int[][] dp = new int[m][n];
-
+        
+        // Initialize the rightmost column and bottom row to 1
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-
-                if (i == 0 && j == 0) {
-                    dp[i][j] = 1;
-                    continue;
-                }
-
-                int up = 0;
-                int left = 0;
-
-                if (i > 0) {
-                    up = dp[i - 1][j];
-                }
-
-                if (j > 0) {
-                    left = dp[i][j - 1];
-                }
-
-                dp[i][j] = up + left;
+            dp[i][n-1] = 1;
+        }
+        for (int j = 0; j < n; j++) {
+            dp[m-1][j] = 1;
+        }
+        
+        // Fill in the DP array bottom-up
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+                dp[i][j] = dp[i+1][j] + dp[i][j+1];
             }
         }
-
-        return dp[m - 1][n - 1];
+        
+        // Return the result stored in the top-left corner
+        return dp[0][0];
     }
 }
